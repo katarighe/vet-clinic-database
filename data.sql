a713458a3187f0ea490a8220913a683dd5d6ea05
katarighe <m.ighe@hotmail.com>
2023-09-18 15:10:20 +0300

    Add comment on the item that I created

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-/* Populate database with sample data. */
+/* Inserted the following data of the 4 animals */
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES
     ('Agumon', '2020-02-03', 0, true, 10.23),
