1fd1fb2e0d5b2948de5b3daac855f4b139e039fe
katarighe <m.ighe@hotmail.com>
2023-09-18 14:08:33 +0300

    Create SQL database schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE DATABASE vet_clinic;
+
+CREATE TABLE animals (  
+    id INT GENERATED ALWAYS AS IDENTITY,
+    create_time DATE,
+    name VARCHAR(255),
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg DECIMAL(5,2),
+    PRIMARY KEY(id)
 );
