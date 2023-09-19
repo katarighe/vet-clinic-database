46b43e6b69149838c720aa8739e972b4c49adbce
katarighe <m.ighe@hotmail.com>
2023-09-19 11:55:32 +0300

    Set the species column to unspecified

--- queries.sql
+++ queries.sql
@@ -22,7 +22,7 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 /* Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg) */
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
-/* Find all animals not named Gabumon. */
+/*  Set the species column to unspecified */
 BEGIN;
 UPDATE animals SET species = 'unspecified' ;
 select name, species from animals;
