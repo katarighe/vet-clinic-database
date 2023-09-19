6c5a6bda52b844117a46e2a6b174b23435ba1465
katarighe <m.ighe@hotmail.com>
2023-09-19 11:57:15 +0300

    Update the animals table on species

--- queries.sql
+++ queries.sql
@@ -28,7 +28,8 @@ UPDATE animals SET species = 'unspecified' ;
 select name, species from animals;
 ROLLBACK;
 
-/* Find all animals not named Gabumon. */
+/* Update the animals table by setting the species column to digimon for all animals that have a name ending in mon. */
+/* Update the animals table by setting the species column to pokemon for all animals that don't have species already set. */
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
