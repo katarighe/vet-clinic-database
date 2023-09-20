43a7686de3cb0f55e397ab8e888409dc5309587d
katarighe <m.ighe@hotmail.com>
2023-09-20 15:18:31 +0300

    Fix foreign key on the species table

--- schema.sql
+++ schema.sql
@@ -39,8 +39,8 @@ ALTER TABLE animals
 DROP COLUMN species;
 
 /* Add column species_id which is a foreign key referencing species table */
-ALTER TABLE animals
-ADD COLUMN species_id integer REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN species_id INT; 
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
 
 /* Add column owner_id which is a foreign key referencing the owners table */
 ALTER TABLE animals ADD COLUMN owner_id INT;
